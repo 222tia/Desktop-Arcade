47ae92e32f8969d03356d8a0a18266067dfbcb10
Tia Self <tiaself2002@gmail.com>
2023-03-16 14:45:59 -0400

    Add UserPlayReceiver and TestUserPlayReceiver class Tia Self <tiamarie.self@bsu.edu>

---
+++ src/main/java/edu/bsu/cs222/finalproject/UserPlayReceiver.java
@@ -0,0 +1,19 @@
+package edu.bsu.cs222.finalproject;
+
+import java.util.Scanner;
+
+public class UserPlayReceiver {
+    public static String getUserPlay() {
+
+        System.out.println("rock, paper, or scissors?: ");
+
+        Scanner userPlayScanner = new Scanner(System.in);
+        String userPlay = userPlayScanner.nextLine().toLowerCase();
+
+        if (!userPlay.matches("rock|paper|scissors")) {
+            System.err.println("invalid input (check spelling)");
+        }
+        return userPlay;
+    }
+}
+
