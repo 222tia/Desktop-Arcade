1b2f100ea34c2705fef15bd5ef41c97e7e9af2ef
helen-6545 <122414895+helen-6545@users.noreply.github.com>
2023-03-18 13:13:20 -0400

    Edited bunco game

---
+++ src/main/java/edu/bsu/cs222/finalproject/BuncoClasses.java
@@ -0,0 +1,9 @@
+package edu.bsu.cs222.finalproject;
+
+public class BuncoClasses {
+
+    public static Integer DiceRoll(){
+        return (int) (Math.random() * 6) + 1;
+    }
+
+}
--- src/main/java/edu/bsu/cs222/finalproject/BuncoGame.java
+++ src/main/java/edu/bsu/cs222/finalproject/BuncoGame.java
@@ -1,6 +1,9 @@
 package edu.bsu.cs222.finalproject;
 
+import java.io.IOException;
+
 public class BuncoGame {
+
     public static void main(String[] args) throws IOException {
         int dice1Value = 0;
         int dice2Value = 0;
@@ -12,54 +15,49 @@ public class BuncoGame {
 
 
         while (playerScore < 21) {
-            for (roundNumber++;){
 
-                do {
-                    System.out.println("\nRound #" + roundNumber);
-                    dice1Value = (int) (Math.random() * 6) + 1;
-                    dice2Value = (int) (Math.random() * 6) + 1;
-                    dice3Value = (int) (Math.random() * 6) + 1;
-                    System.out.println("Roll 1 = " + dice1Value);
-                    System.out.println("Roll 2 = " + dice2Value);
-                    System.out.println("Roll 3 = " + dice3Value);
+            boolean turnContinue;
+            do {
+                System.out.println("\nRound #" + roundNumber);
+                dice1Value = BuncoClasses.DiceRoll();
+                dice2Value = BuncoClasses.DiceRoll();
+                dice3Value = BuncoClasses.DiceRoll();
+                System.out.println("Roll 1 = " + dice1Value);
+                System.out.println("Roll 2 = " + dice2Value);
+                System.out.println("Roll 3 = " + dice3Value);
 
 
-                    if (dice1Value == roundNumber) {
-                        playerScore = playerScore + 1;
-                    }
-                    if (dice2Value == roundNumber) {
-                        playerScore = playerScore + 1;
-                    }
-                    if (dice3Value == roundNumber) {
-                        playerScore = playerScore + 1;
-                    }
-                    if ((dice1Value == dice2Value) && (dice2Value == dice3Value)) {
-                        playerScore = playerScore + 5;
-                    }
-
-                    if ((dice1Value == dice2Value) && (dice2Value == dice3Value) && (dice1Value == roundNumber)) {
+                boolean pointGain = (dice1Value == roundNumber) || (dice2Value == roundNumber) || (dice3Value == roundNumber);
+                if (pointGain) playerScore = playerScore + 1;
+                boolean DiceTriples = false;
+                if ((dice1Value == dice2Value) && (dice2Value == dice3Value)) {
+                    DiceTriples = true;
+                    playerScore = playerScore + 5;
+                    if (pointGain) {
                         playerScore = playerScore + 21;
-                        System.out.println("Bunco!! You Win!");
-                        System.exit(0);
                     }
-
-                    System.out.println("Your score is " + playerScore);
-
-                    if (playerScore >= 21) {
-                        System.out.println("Bunco! You win!");
-                        System.exit(0);
-                    }
-
-                    mykey = (int) System.in.read();
-                    mykey = (int) System.in.read();
-
-
-                } while ((dice1Value == roundNumber) || (dice2Value == roundNumber) || (dice3Value == roundNumber) || ((dice1Value == dice2Value) && (dice2Value == dice3Value)));
-                if (roundNumber == 6) {
-                    roundNumber = 0;
                 }
+                turnContinue = false;
+                if (pointGain || DiceTriples) {
+                    turnContinue = true;
+                }
+                System.out.println("Your score is " + playerScore);
+
+                if (playerScore >= 21) {
+                    System.out.println("Bunco! You win!");
+                    System.exit(0);
+                }
+
+                mykey = (int) System.in.read();
+                mykey = (int) System.in.read();
+
+
+            } while (turnContinue);
+            roundNumber++;
+            if (roundNumber == 6) {
+                roundNumber = 0;
             }
+        }
         }
 
     }
-}
