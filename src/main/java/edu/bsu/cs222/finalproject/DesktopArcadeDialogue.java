df1ba43bba95fd3f568f3618cdbadada8e8868e1
jay <emma.jordan@bsu.edu>
2023-03-19 15:34:06 -0400

    Insertion of Dialogue into DesktopArcadeDialogue

--- src/main/java/edu/bsu/cs222/finalproject/DesktopArcadeDialogue.java
+++ src/main/java/edu/bsu/cs222/finalproject/DesktopArcadeDialogue.java
@@ -1,4 +1,17 @@
 package edu.bsu.cs222.finalproject;
 
 public class DesktopArcadeDialogue {
+    public static void startUpDialogue(){
+        System.out.println("Welcome to Desktop Arcade" +
+                "To play, please enter the number of the corresponding game:");
+        System.out.println("1.) Rock Paper Scissors!" +
+                "2.) Bunco!");
+    }
+    public static void RPSRules(){
+        System.out.println("dialogue1");
+    }
+    public static void BuncoRules(){
+        System.out.println("dialogue2");
+    }
 }
+
