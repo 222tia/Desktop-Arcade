5a31a822405b74dd60668ca29c2f5d859330dca9
jay <emma.jordan@bsu.edu>
2023-03-19 17:02:24 -0400

    Creates Loop in PlayDesktopArcade

--- src/main/java/edu/bsu/cs222/finalproject/PlayDesktopArcade.java
+++ src/main/java/edu/bsu/cs222/finalproject/PlayDesktopArcade.java
@@ -3,20 +3,22 @@ package edu.bsu.cs222.finalproject;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class PlayDesktopArcade {
+public class PlayDesktopArcade extends DesktopArcadeDialogue{
     public static void main(String[] args) throws IOException {
         Scanner userChoice = new Scanner(System.in);
+        do {
+            DesktopArcadeDialogue.startUpDialogue();
 
-        DesktopArcadeDialogue.startUpDialogue();
-
-        if (userChoice.nextLine().equals("1")){
-            DesktopArcadeDialogue.RPSRules();
-            RPSGame.playRPS();
-        }if (userChoice.nextLine().equals("2")){
-            DesktopArcadeDialogue.BuncoRules();
-            BuncoGame.playBunco();
-        }else{
-            DesktopArcadeDialogue.incorrectInput();
-        }
+            if (userChoice.nextLine().equals("1")) {
+                DesktopArcadeDialogue.RPSRules();
+                RPSGame.playRPS();
+            }
+            if (userChoice.nextLine().equals("2")) {
+                DesktopArcadeDialogue.BuncoRules();
+                BuncoGame.playBunco();
+            } else {
+                DesktopArcadeDialogue.incorrectInput();
+            }
+        }while(!userChoice.nextLine().equals("1")&& !userChoice.nextLine().equals("2"));
     }
 }
