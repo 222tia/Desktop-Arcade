47e3e855a6df5f666a06bd04f714aa2695b12729
Tia Self <tiaself2002@gmail.com>
2023-03-19 23:43:01 -0400

    Update BuncoGame class to use getDiceRolls method Tia Self <tiamarie.self@bsu.edu>

--- src/main/java/edu/bsu/cs222/finalproject/BuncoGame.java
+++ src/main/java/edu/bsu/cs222/finalproject/BuncoGame.java
@@ -1,8 +1,11 @@
 package edu.bsu.cs222.finalproject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BuncoGame {
+    static List<Integer> diceRollList = new ArrayList<>();
     public static void playBunco() throws IOException {
 
         int playerScore = 0;
@@ -15,12 +18,16 @@ public class BuncoGame {
             boolean turnContinue;
             do {
                 System.out.println("\nRound #" + roundNumber);
-                int dice1Value = BuncoDice.DiceRoll();
-                int dice2Value = BuncoDice.DiceRoll();
-                int dice3Value = BuncoDice.DiceRoll();
-                System.out.println("Roll 1 = " + dice1Value);
-                System.out.println("Roll 2 = " + dice2Value);
-                System.out.println("Roll 3 = " + dice3Value);
+
+                diceRollList = BuncoDice.getDiceRolls();
+
+                int dice1Value = diceRollList.get(0);
+                int dice2Value = diceRollList.get(1);
+                int dice3Value = diceRollList.get(2);
+
+                System.out.println("Roll 1 = " + dice1Value +
+                        "\n Roll 2 = " + dice2Value +
+                        "\n Roll 3 = " + dice3Value );
 
                 boolean pointGain = ((dice1Value== roundNumber) || (dice2Value== roundNumber) || (dice3Value == roundNumber));
                 boolean DiceTriples = (dice1Value == dice2Value) && (dice2Value == dice3Value);
