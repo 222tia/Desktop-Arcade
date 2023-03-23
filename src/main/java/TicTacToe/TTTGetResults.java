2f03642d32d0abcfe7432a80811bec8c5cf1623c
Tia Self <tiaself2002@gmail.com>
2023-03-23 12:51:30 -0400

    Add TTTGetResults class

---
+++ src/main/java/TicTacToe/TTTGetResults.java
@@ -0,0 +1,9 @@
+package TicTacToe;
+
+public class TTTGetResults {
+    public static boolean checkBoard(){
+        // returns true if one of the win conditions is met
+        // use this to exit and print results in another method
+        return true;
+    }
+}
