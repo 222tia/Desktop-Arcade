3d691bae4b82b9f7c094e5d3c33af4c7c1c77c14
Tia Self <tiaself2002@gmail.com>
2023-03-23 13:08:14 -0400

    Add TTTPlayRandomizer class

---
+++ src/main/java/TicTacToe/TTTPlayRandomizer.java
@@ -0,0 +1,14 @@
+package TicTacToe;
+
+import java.util.List;
+
+public class TTTPlayRandomizer {
+    private final static List<Integer> possibleInputs = List.of(1, 2, 3, 4, 5, 6, 7 ,8 ,9);
+
+    public static int getRandomPlay() {
+
+        System.out.println("\nComputer's turn.");
+        return (int) (Math.random() * 9);
+
+    }
+}
