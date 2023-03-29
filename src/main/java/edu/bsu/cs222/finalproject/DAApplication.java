81c6a2e4a50d211d36188831808ed9fef85731f2
jay <emma.jordan@bsu.edu>
2023-03-29 17:17:35 -0400

    Updates class names DAApplication and DAController

---
+++ src/main/java/edu/bsu/cs222/finalproject/DAApplication.java
@@ -0,0 +1,11 @@
+package edu.bsu.cs222.finalproject;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class DAApplication extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
--- src/main/java/edu/bsu/cs222/finalproject/DAController.java
+++ src/main/java/edu/bsu/cs222/finalproject/DAController.java
@@ -4,7 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class DAGUIController {
+public class DAController {
     @FXML
     public Label welcomeText;
 
