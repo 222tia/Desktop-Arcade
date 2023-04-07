6dfaa4b89c8c48b8a9d717f0aadb29249a52a5eb
Tia Self <tiaself2002@gmail.com>
2023-04-07 12:20:57 -0400

    Changes to RPS GUI

--- src/main/java/edu/bsu/cs222/GUIRPSPlay.java
+++ src/main/java/edu/bsu/cs222/GUIRPSPlay.java
@@ -1,4 +1,30 @@
 package edu.bsu.cs222;
 
-public class GUIRPSPlay {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GUIRPSPlay implements Initializable {
+
+    @FXML
+    private Label userPlayPrompt;
+    @FXML
+    private ChoiceBox<String> userPlayChoice;
+
+    private final String[] playChoices = {"rock", " paper", "scissors"};
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        userPlayChoice.getItems().addAll(playChoices);
+    }
+
+    @FXML
+    public void onPlayClick() throws IOException {
+
+    }
 }
