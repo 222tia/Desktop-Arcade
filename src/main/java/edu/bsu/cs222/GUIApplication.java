59ec06c7af9d2170d586e00dbb4416b02cc4311c
jay <emma.jordan@bsu.edu>
2023-04-09 21:40:08 -0400

    Gets rid of unnecessary method calls

--- src/main/java/edu/bsu/cs222/GUIApplication.java
+++ src/main/java/edu/bsu/cs222/GUIApplication.java
@@ -11,7 +11,7 @@ public class GUIApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GUIApplication.class.getResource("Hub-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 550);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/edu/bsu/cs222/GUIController.java
+++ src/main/java/edu/bsu/cs222/GUIController.java
@@ -2,48 +2,42 @@ package edu.bsu.cs222;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
-public class GUIController implements Initializable {
+import java.io.IOException;
+
+public class GUIController {
     @FXML
     public Label homeName;
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-
-    }
 
     @FXML
     public void onRPSClick() throws IOException {
-            FXMLLoader RPSLoader = new FXMLLoader(getClass().getResource("RPS-play.fxml"));
-            Parent root1 = RPSLoader.load();
-            Stage stage = new Stage();
-            stage.setScene(new Scene(root1));
-            stage.show();
-    }
-
-    @FXML
-    protected void onBuncoClick() throws IOException {
-        FXMLLoader BuncoLoader = new FXMLLoader(getClass().getResource("Bunco-play.fxml"));
-        Parent root1 = BuncoLoader.load();
+        FXMLLoader RPSLoader = new FXMLLoader(getClass().getResource("RPS-play.fxml"));
+        Parent RPSroot = RPSLoader.load();
         Stage stage = new Stage();
-        stage.setScene(new Scene(root1));
+        stage.setScene(new Scene(RPSroot));
         stage.show();
     }
 
     @FXML
-    protected void onTTTClick() throws IOException {
-        FXMLLoader TTTLoader = new FXMLLoader(getClass().getResource("TTT-play.fxml"));
-        Parent root1 = TTTLoader.load();
+    public void onBuncoClick() throws IOException {
+        FXMLLoader BuncoLoader = new FXMLLoader(getClass().getResource("Bunco-play.fxml"));
+        Parent Buncoroot = BuncoLoader.load();
         Stage stage = new Stage();
-        stage.setScene(new Scene(root1));
+        stage.setScene(new Scene(Buncoroot));
+        stage.show();
+    }
+
+    @FXML
+    public void onTTTClick() throws IOException {
+        FXMLLoader TTTLoader = new FXMLLoader(getClass().getResource("TTT-play.fxml"));
+        Parent TTTroot = TTTLoader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(TTTroot));
         stage.show();
 
     }
