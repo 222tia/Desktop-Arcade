977a886ce8df82362ca9384a34df9f0508b65b88
helen-6545 <122414895+helen-6545@users.noreply.github.com>
2023-04-09 20:38:14 -0400

    Revert "Tests fix of duplication issue"
    This reverts commit 59614493f9340297a1386d07adffc2a03450f67c.

---
+++ src/test/java/edu/bsu/cs222/TestRPSScoreKeeper.java
@@ -0,0 +1,157 @@
+package edu.bsu.cs222;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestRPSScoreKeeper {
+
+    @Test
+    public void testUserScoreGainFrom0() {
+        final String userPlay = "paper";
+        final String computerPlay = "rock";
+        final int userScore = 0;
+        Assertions.assertEquals(1, RPSScoreKeeper.addUserScore(computerPlay, userPlay, userScore));
+    }
+
+    @Test
+    public void testUserScoreGainFrom1() {
+        final String userPlay = "scissors";
+        final String computerPlay = "paper";
+        final int userScore = 1;
+        Assertions.assertEquals(2, RPSScoreKeeper.addUserScore(computerPlay, userPlay, userScore));
+    }
+
+    @Test
+    public void testUserScoreGainFrom2() {
+        final String userPlay = "rock";
+        final String computerPlay = "scissors";
+        final int userScore = 2;
+        Assertions.assertEquals(3, RPSScoreKeeper.addUserScore(computerPlay, userPlay, userScore));
+    }
+
+    @Test
+    public void testNoUserScoreRock() {
+        final String userPlay = "rock";
+        final String computerPlay = "paper";
+        final int userScore = 0;
+        Assertions.assertEquals(0, RPSScoreKeeper.addUserScore(computerPlay, userPlay, userScore));
+    }
+
+    @Test
+    public void testNoUserScoreGainPaper() {
+        final String userPlay = "paper";
+        final String computerPlay = "scissors";
+        final int userScore = 0;
+        Assertions.assertEquals(0, RPSScoreKeeper.addUserScore(computerPlay, userPlay, userScore));
+    }
+
+    @Test
+    public void testNoUserScoreGainScissors() {
+        final String userPlay = "scissors";
+        final String computerPlay = "rock";
+        final int userScore = 0;
+        Assertions.assertEquals(0, RPSScoreKeeper.addUserScore(computerPlay, userPlay, userScore));
+    }
+
+    @Test
+    public void testComputerScoreGainFrom0() {
+        final String userPlay = "rock";
+        final String computerPlay = "paper";
+        final int computerScore = 0;
+        Assertions.assertEquals(1, RPSScoreKeeper.addComputerScore(computerPlay, userPlay, computerScore));
+    }
+
+    @Test
+    public void testComputerScoreGainFrom1() {
+        final String userPlay = "paper";
+        final String computerPlay = "scissors";
+        final int computerScore = 1;
+        Assertions.assertEquals(2, RPSScoreKeeper.addComputerScore(computerPlay, userPlay, computerScore));
+    }
+
+    @Test
+    public void testComputerScoreGainFrom2() {
+        final String userPlay = "scissors";
+        final String computerPlay = "rock";
+        final int computerScore = 2;
+        Assertions.assertEquals(3, RPSScoreKeeper.addComputerScore(computerPlay, userPlay, computerScore));
+    }
+
+    @Test
+    public void testComputerScoreRock() {
+        final String userPlay = "paper";
+        final String computerPlay = "rock";
+        final int computerScore = 0;
+        Assertions.assertEquals(0, RPSScoreKeeper.addComputerScore(computerPlay, userPlay, computerScore));
+    }
+
+    @Test
+    public void testComputerScoreGainPaper() {
+        final String userPlay = "scissors";
+        final String computerPlay = "paper";
+        final int computerScore = 0;
+        Assertions.assertEquals(0, RPSScoreKeeper.addComputerScore(computerPlay, userPlay, computerScore));
+    }
+
+    @Test
+    public void testNoComputerScoreGainScissors() {
+        final String userPlay = "rock";
+        final String computerPlay = "scissors";
+        final int computerScore = 0;
+        Assertions.assertEquals(0, RPSScoreKeeper.addComputerScore(computerPlay, userPlay, computerScore));
+    }
+
+    @Test
+    public void testUserWinsBy2(){
+        final int computerScore = 0;
+        final int userScore = 2;
+        Assertions.assertTrue(RPSScoreKeeper.checkScore(userScore, computerScore));
+    }
+
+    @Test
+    public void testUserWinsBy1(){
+        final int computerScore = 2;
+        final int userScore = 3;
+        Assertions.assertTrue(RPSScoreKeeper.checkScore(userScore, computerScore));
+    }
+    @Test
+    public void testComputerWinsBy2(){
+        final int computerScore = 2;
+        final int userScore = 0;
+        Assertions.assertTrue(RPSScoreKeeper.checkScore(computerScore, userScore));
+    }
+
+    @Test
+    public void testComputerWinsBy1(){
+        final int computerScore = 3;
+        final int userScore = 2;
+        Assertions.assertTrue(RPSScoreKeeper.checkScore(computerScore, userScore));
+    }
+    @Test
+    public void testNoWinnerEvenScore_ComputerScoreFirst(){
+        final int computerScore = 2;
+        final int userScore = 2;
+        Assertions.assertFalse(RPSScoreKeeper.checkScore(computerScore, userScore));
+    }
+
+    @Test
+    public void testNoWinnerEvenScore_UserScoreFirst(){
+        final int computerScore = 2;
+        final int userScore = 2;
+        Assertions.assertFalse(RPSScoreKeeper.checkScore(userScore, computerScore));
+    }
+
+    @Test
+    public void testNoWinnerUserScore1(){
+        final int computerScore = 0;
+        final int userScore = 1;
+        Assertions.assertFalse(RPSScoreKeeper.checkScore(userScore, computerScore));
+    }
+
+    @Test
+    public void testNoWinnerComputerScore1(){
+        final int computerScore = 0;
+        final int userScore = 1;
+        Assertions.assertFalse(RPSScoreKeeper.checkScore(computerScore, userScore));
+    }
+}
---
+++ src/test/java/edu/bsu/cs222/TestTTTGetResults.java
@@ -0,0 +1,64 @@
+package edu.bsu.cs222;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TestTTTGetResults {
+
+    @Test
+    public void testFirstRowWinX(){
+        ArrayList<String> gameBoard = new ArrayList<>(Arrays.asList("X", "X", "X", " ", " ", " ", " ", " ", " "));
+        String letter = "X";
+        Assertions.assertTrue(TTTGetResults.checkBoard(letter, gameBoard));
+    }
+
+    @Test
+    public void testFirstRowWinO(){
+        ArrayList<String> gameBoard = new ArrayList<>(Arrays.asList("O", "O", "O", " ", " ", " ", " ", " ", " "));
+        String letter = "O";
+        Assertions.assertTrue(TTTGetResults.checkBoard(letter, gameBoard));
+    }
+
+    @Test
+    public void testFirstRowWinFalse(){
+        ArrayList<String> gameBoard = new ArrayList<>(Arrays.asList("O", " ", "O", " ", "X", " ", " ", " ", "X"));
+        String letter = "O";
+        Assertions.assertFalse(TTTGetResults.checkBoard(letter, gameBoard));
+    }
+
+    @Test
+    public void testDiagonalWinX(){
+        ArrayList<String> gameBoard = new ArrayList<>(Arrays.asList("X", " ", " ", " ", "X", " ", " ", " ", "X"));
+        String letter = "X";
+        Assertions.assertTrue(TTTGetResults.checkBoard(letter, gameBoard));
+    }
+
+    @Test
+    public void testDiagonalWinO(){
+        ArrayList<String> gameBoard = new ArrayList<>(Arrays.asList(" ", " ", "O", " ", "O", " ", "O", " ", " "));
+        String letter = "O";
+        Assertions.assertTrue(TTTGetResults.checkBoard(letter, gameBoard));
+    }
+
+    @Test
+    public void testDiagonalWinFalse(){
+        ArrayList<String> gameBoard = new ArrayList<>(Arrays.asList(" ", " ", "X", " ", "O", " ", "O", "X", " "));
+        String letter = "O";
+        Assertions.assertFalse(TTTGetResults.checkBoard(letter, gameBoard));
+    }
+
+    @Test
+    public void testDrawTrue(){
+        ArrayList<String> gameBoard = new ArrayList<>(Arrays.asList("X", "X", "O", "O", "O", "X", "X", "O", "X"));
+        Assertions.assertTrue(TTTGetResults.checkDraw(gameBoard));
+    }
+
+    @Test
+    public void testDrawFalse(){
+        ArrayList<String> gameBoard = new ArrayList<>(Arrays.asList("X", " ", "X", "O", "O", "X", "X", "O", "X"));
+        Assertions.assertFalse(TTTGetResults.checkDraw(gameBoard));
+    }
+}
