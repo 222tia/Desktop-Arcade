52fee3d9c8d066a8eed6f656da0c285a268a50ec
jay <emma.jordan@bsu.edu>
2023-04-11 15:25:36 -0400

    Visual tweaks to GUI

--- src/main/java/edu/bsu/cs222/GUIApplication.java
+++ src/main/java/edu/bsu/cs222/GUIApplication.java
@@ -1,13 +1,20 @@
 package edu.bsu.cs222;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javax.swing.text.html.ImageView;
+import java.awt.*;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
+import java.io.File;
 import java.io.IOException;
 
 public class GUIApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GUIApplication.class.getResource("Hub-view.fxml"));
