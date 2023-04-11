6b5e64cae8b0c3b9ad86e3f1a02b76702f10419b
helen-6545 <122414895+helen-6545@users.noreply.github.com>
2023-04-11 13:31:53 -0400

    Works on Bunco GUI

--- src/main/java/edu/bsu/cs222/GUIBuncoDialogue.java
+++ src/main/java/edu/bsu/cs222/GUIBuncoDialogue.java
@@ -1,7 +1,35 @@
 package edu.bsu.cs222;
 
 public class GUIBuncoDialogue {
-    public static Object RollOutput(int dice1, int dice2, int dice3) {
-        return ("Roll 1 = " + dice1 + "\nRoll 2 = " + dice2 + "\nRoll 3 = " + dice3);
+    public static String RollOutput(int dice1, int dice2, int dice3) {
+        return ("\nRoll 1 = " + dice1 + "\nRoll 2 = " + dice2 + "\nRoll 3 = " + dice3);
     }
+
+    public static String GUIRoundDisplay(int round){
+        return("\nRound #" + round);
+    }
+
+    public static String PlayerTurnDisplay(){
+        return("It's Your Turn!");
+    }
+
+    public static String ScoreDisplay(int playerScore, int compScore){
+        return("Your score is "+playerScore+". Your opponent's score is "+compScore+".");
+    }
+
+    public static String PlayerContinueDisplay(){
+        return("\n\nYou gained points, so you get to roll again!");
+    }
+    public static String CompContinueDisplay(){return("\n\nYour opponent gained points, and get to roll again!");}
+
+    public static String PlayerWinDisplay(){
+        return("\n\nBunco! You Win!");
+    }
+    public static String CompWinDisplay(){
+        return("\n\nYou Lose! Better luck next time!");
+    }
+
+    public static String CompTurnDisplay() {return("It's Your Opponent's Turn!"); }
+
+    public static String RestartDisplay() {return("Please restart the game");}
 }
--- src/main/java/edu/bsu/cs222/GUIBuncoPlay.java
+++ src/main/java/edu/bsu/cs222/GUIBuncoPlay.java
@@ -1,6 +1,7 @@
 package edu.bsu.cs222;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 
 
 import java.awt.*;
@@ -11,46 +12,71 @@ import java.util.List;
 public class GUIBuncoPlay {
 
     static List<Integer> diceRollList = new ArrayList<>();
+    public TextArea buncoTextArea;
+    public TextArea buncoScoreArea;
+    public int playerScore = 0;
+    public int compScore = 0;
+    public int roundNumber = 1;
+    public int turnOrder = 1;
+
     @FXML
     public void onBuncoDiceClick() throws IOException {
-            int playerScore = 0;
-            int compScore = 0;
-            int roundNumber = 1;
 
 
+        if((playerScore<21)&&(compScore<21)) {
+            buncoScoreArea.setText(GUIBuncoDialogue.ScoreDisplay(playerScore, compScore));
+            if (turnOrder == 1) {
+                buncoTextArea.setText(GUIBuncoDialogue.PlayerTurnDisplay());
+            } else {
+                buncoTextArea.setText(GUIBuncoDialogue.CompTurnDisplay());
+            }
+            boolean turnContinue;
+            buncoTextArea.appendText(GUIBuncoDialogue.GUIRoundDisplay(roundNumber));
 
-                BuncoDialogue.PlayerTurnDisplay();
-                boolean turnContinue;
-                    BuncoDialogue.RoundDisplay(roundNumber);
+            diceRollList = BuncoDice.getDiceRolls();
 
-                    diceRollList = BuncoDice.getDiceRolls();
+            int dice1Value = diceRollList.get(0);
+            int dice2Value = diceRollList.get(1);
+            int dice3Value = diceRollList.get(2);
+            buncoTextArea.appendText(GUIBuncoDialogue.RollOutput(dice1Value, dice2Value, dice3Value));
 
-                    int dice1Value = diceRollList.get(0);
-                    int dice2Value = diceRollList.get(1);
-                    int dice3Value = diceRollList.get(2);
-                    Label buncoText = new Label(GUIBuncoDialogue.RollOutput(dice1Value, dice2Value, dice3Value).toString());
-                    //BuncoDialogue.DiceOutput(dice1Value, dice2Value, dice3Value);
+            boolean pointGain = ((dice1Value == roundNumber) || (dice2Value == roundNumber) || (dice3Value == roundNumber));
+            boolean DiceTriples = (dice1Value == dice2Value) && (dice2Value == dice3Value);
+            playerScore = BuncoDice.Scoring(playerScore, roundNumber, diceRollList);
 
-                    boolean pointGain = ((dice1Value == roundNumber) || (dice2Value == roundNumber) || (dice3Value == roundNumber));
-                    boolean DiceTriples = (dice1Value == dice2Value) && (dice2Value == dice3Value);
-                    playerScore = BuncoDice.Scoring(playerScore, roundNumber, diceRollList);
+            turnContinue = pointGain || DiceTriples;
 
-                    turnContinue = pointGain || DiceTriples;
+            buncoScoreArea.setText(GUIBuncoDialogue.ScoreDisplay(playerScore, compScore));
+            diceRollList.clear();
 
-                    BuncoDialogue.PlayerScoreDisplay(playerScore);
-                    diceRollList.clear();
+            if (playerScore >= 21) {
+                buncoTextArea.appendText(GUIBuncoDialogue.PlayerWinDisplay());
+            }
+            if (compScore >= 21) {
+                buncoTextArea.appendText(GUIBuncoDialogue.CompWinDisplay());
+            }
 
-                    if (playerScore >= 21) {
-                        BuncoDialogue.PlayerWinDisplay();
-                    }
-
-                    DesktopArcadeDialogue.EnterContinue();
-
-                compScore = BuncoCompTurn.CompTurn(compScore, roundNumber);
-                roundNumber++;
-                if (roundNumber > 6) {
-                    roundNumber = 1;
+            if ((turnContinue) && (turnOrder == 1)) {
+                buncoTextArea.appendText(GUIBuncoDialogue.PlayerContinueDisplay());
+            }
+            if ((turnContinue) && (turnOrder == 2)) {
+                buncoTextArea.appendText(GUIBuncoDialogue.CompContinueDisplay());
+            } else {
+                if (turnOrder == 1) {
+                    turnOrder = 2;
+                } else {
+                    turnOrder = 1;
                 }
+            }
+
+            roundNumber++;
+            if (roundNumber > 6) {
+                roundNumber = 1;
+            }
+        }
+        else{
+            buncoTextArea.appendText(GUIBuncoDialogue.RestartDisplay());
+        }
             }
         }
 
