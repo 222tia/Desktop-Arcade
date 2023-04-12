2200b404b03961d152efcc42b43a477a6a52a1af
helen-6545 <122414895+helen-6545@users.noreply.github.com>
2023-04-12 14:46:07 -0400

    Edits TTT rules

--- src/main/java/edu/bsu/cs222/GUITTTDialogue.java
+++ src/main/java/edu/bsu/cs222/GUITTTDialogue.java
@@ -70,6 +70,11 @@ public class GUITTTDialogue {
                 Tic-Tac-Toe is played by using a 3x3 grid.
                 Players take turns to select a spot in the grid to put an X or an O.
                 If a player gets 3 in a row vertically, horizontally, or diagonally they win!
-                Enter a number 1-9 and press enter to play.""";
+                Enter a number 1-9 to choose your space.
+                              1 | 2 | 3
+                             ------------
+                              4 | 5 | 6
+                             ------------
+                              7 | 8 | 9""";
     }
 }
