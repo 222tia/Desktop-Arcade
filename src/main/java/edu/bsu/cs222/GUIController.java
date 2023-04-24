ee32ca47b4fba65907451f3dcbe3c09479e2c172
jay <emma.jordan@bsu.edu>
2023-04-24 16:13:22 -0400

    Fixes warnings

--- src/main/java/edu/bsu/cs222/GUIController.java
+++ src/main/java/edu/bsu/cs222/GUIController.java
@@ -5,11 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class GUIController {
+    @FXML
+    public VBox hubView;
     @FXML
     public Label homeName;
 
