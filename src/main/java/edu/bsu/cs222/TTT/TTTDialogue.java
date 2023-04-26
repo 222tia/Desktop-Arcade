7d91431aad7b8661cf29d8901813a3a15322f133
Tia Self <tiaself2002@gmail.com>
2023-04-26 18:45:53 -0400

    Remove unused methods

--- src/main/java/edu/bsu/cs222/TTT/TTTDialogue.java
+++ src/main/java/edu/bsu/cs222/TTT/TTTDialogue.java
@@ -104,10 +104,6 @@ public class TTTDialogue {
                                              7 | 8 | 9""";
     }
 
-    public static String player1win() {return("\n\n\n Player 1 Wins");}
-
-    public static String player2win() {return("\n\n\n Player 2 Wins");}
-
     public static String player1Turn() {return("Player 1, enter your selection and hit enter");}
 
     public static String player2Turn() {return("Player 2, enter your selection and hit enter");}
